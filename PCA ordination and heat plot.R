fb0c330cb8383163dbfbc70b61f0af0e5227daa8
Erik Olsen <eriko@imr.no>
2016-03-15 17:08:53 +0100

    R script for PCA and Het plots
    Script used for WGINOSE 2016 report.

---
+++ PCA ordination and heat plot.R
@@ -0,0 +1,82 @@
+#' @title  PCA based Integrated Ecological analysis of the North Sea
+#' @description  Script used by WGINOSE for 2016 report
+#' @description  Based on earlier script developed by Andrew Kerry, CEFAS, UK        
+#' and produce a 'shade' plot  of the original data matrix (sorted against PC1)
+
+
+#' LIBRARIES
+#' ---------------------
+library (lattice)
+
+#' SETTING WORKING DIRECTORIES
+#' ---------------------
+#' setwd("/TEMP/WGINOSE 2016")
+setwd("~/ownCloud/Research/WGINOSE/PCA IEA analysis") #erik local files
+
+
+#' looping through all WGINOSE AREAS
+#' ---------------------
+
+area<-c(1:11)
+area.names<-c("Orkney-Shetland", "Fladen", "Utsira", "Long Forties", "Dogger Bank", "Norfolk Banks", "German Bight", "Oyster Ground", "Southern Bight", "Skagerrak", "Kattegat" )
+# Add area names to the plots and file names
+
+for (i in 1:length(area)){ 
+
+  
+  #' IMPORTING  & TRANSFORMING DATA
+  #' ---------------------
+  data <- as.matrix(read.csv ("COMBO_test.csv", row.names=1, header=TRUE))
+  
+  trans.std <- as.data.frame(scale(log10(data+1)))
+  
+  
+  #' ANALYSES
+  #' ---------------------
+  #' Perform PCA
+  
+  data.pca <- prcomp(trans.std, cor = F)
+  summary(data.pca)
+  
+  
+  #' Extract loadings (eigenvalues) for PC1 & PC2 vectors
+  
+  pc1 <- data.pca$rotation[,1]
+  pc2 <- data.pca$rotation[,2]
+  loadings <- data.frame(pc1,pc2)
+  pc1.order <- order(pc1, na.last = TRUE, decreasing = TRUE)
+  loadings.order <- loadings[pc1.order,]
+  loadings.order
+  
+  write.csv(loadings.order, file = paste("WGINOSE16_Varloadings_",area.names[i], area[i], ".csv", sep=""))
+  
+  
+  #' Plotordination using PC1 & PC2 scores
+  
+  plot(data.pca$x[,1],data.pca$x[,2], xlab = "PC1", xlim = c(-8,11), ylab = "PC2", type = "both", main ="PCA")
+  text(data.pca$x[,1], data.pca$x[,2], row.names(data), cex=0.9, pos=4, col="red") # add labels
+  
+  pdf(paste("WGINOSE16_PCA_ordniation_", area.names[i], area[i], ".pdf", sep=""))
+  plot(data.pca$x[,1],data.pca$x[,2], xlab = "PC1", xlim = c(-10,11), ylab = "PC2", type = "both", main ="PCA")
+  text(data.pca$x[,1], data.pca$x[,2], row.names(data), cex=0.9, pos=4, col="red") # add labels
+  dev.off()
+  
+  
+  #' reorder the data matrix stnddata using the ranked order of PC1 loadings pc1.order
+  
+  trans.std.order <- as.matrix(trans.std[, pc1.order])
+  
+  #' Heat plot of PCA scores
+  
+  levelplot(trans.std.order, col.regions = rainbow(100, start = 0, end = 0.325), scales = list(x=list(cex=0.8, rot = 90), y=list(cex = 0.7)), main=list(label="PCA Heatplot", cex = 2), xlab=list(label="Year",cex=2), ylab=list(label="Variables",cex=2), aspect = 1.25)
+  
+  pdf(paste("WGINOSE16_PCA_heat", area.names[i], area[i], ".pdf", sep=""))
+  levelplot(trans.std.order, col.regions = rainbow(100, start = 0, end = 0.325), scales = list(x=list(cex=0.8, rot = 90), y=list(cex = 0.4)), main=list(label="PCA Heatplot", cex = 1), xlab=list(label="Year",cex=1), ylab=list(label="Variables",cex=1), aspect = 1.25)
+  dev.off()
+}
+
+ 
+
+
+
+
