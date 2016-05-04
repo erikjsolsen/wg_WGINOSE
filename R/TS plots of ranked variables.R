13cc0b01f00bafd9f1a54f94ac03388ae6969011
Erik Olsen <eriko@imr.no>
2016-05-04 13:11:54 +0200

    Modified for areas 7,8 & 9

--- R/TS plots of ranked variables.R
+++ R/TS plots of ranked variables.R
@@ -14,8 +14,13 @@ library(RColorBrewer)
 #' looping through all WGINOSE AREAS
 #' ---------------------
 
-area<-c(1:11)
-area.names<-c("Orkney-Shetland", "Fladen", "Utsira", "Long Forties", "Dogger Bank", "Norfolk Banks", "German Bight", "Oyster Ground", "Southern Bight", "Skagerrak", "Kattegat" )
+#area<-c(1:11)
+#area.names<-c("Orkney-Shetland", "Fladen", "Utsira", "Long Forties", "Dogger Bank", "Norfolk Banks", "German Bight", "Oyster Ground", "Southern Bight", "Skagerrak", "Kattegat" )
+
+#select only areas w benthos data
+area<-c(7:9)
+area.names<-c("German Bight", "Oyster Ground", "Southern Bight")
+
 # Add area names to the plots and file names
 
 #for (i in 1:length(area)){ 
@@ -30,43 +35,73 @@ for (i in 1:length(area)){  # not the same number of StDev and mean columns
   #' ----------------
   PCAtable<-tbl_df(read.csv(paste("../WGINOSE16_Varloadings_",area.names[i], area[i], ".csv", sep="")))
   CPUEsd<-tbl_df(read.csv(paste("../../data/CPUE st_dev area ",area[i],".csv", sep="")))
+  colnames(CPUEsd)[2]<-c("year")
   CPUEmean<-tbl_df(read.csv(paste("../../data/CPUE mean area ",area[i],".csv", sep="")))
+  colnames(CPUEmean)[2]<-c("year")
+  #Other<-tbl_df(read.csv(paste("../../data/wgi",area[i],"_mean_std.csv", sep="")))
   Other<-tbl_df(read.csv(paste("../../data/wgi",area[i],"_mean_std.csv", sep="")))
   Other<-filter(Other, Year>1983)
+  colnames(Other)[1]<-c("year")
+  
+  benthos<-tbl_df(read.csv("../../data/ISIS_key_species_nha_station_mean_std.csv"))
+  benthos.sel<-filter(benthos, AreaName==area[i])
+  benthos.sel<-arrange(benthos.sel, year)
+  
+  benthos.sel$SCIENTIFIC_NAME<-gsub(" Sp.", "_Sp_", benthos.sel$SCIENTIFIC_NAME)
+  benthos.sel$SCIENTIFIC_NAME<-gsub(" sp.", "_sp_", benthos.sel$SCIENTIFIC_NAME)
+
+  bl<-levels(as.factor((benthos.sel$SCIENTIFIC_NAME)))
+  
+  #select benthos species included in PCA analysis
+  #bl<- bl[c(3,5,6,8,9,11,13)]
+  
+  for (j in 1:length(bl)){ 
+    benthos.sel.sp<-filter(benthos.sel, SCIENTIFIC_NAME==bl[j])
+    
+    Other<-full_join(Other, benthos.sel.sp[,c(2,5,6)], by="year")
+
+    colnames(Other)[c(dim(Other)[2]-1,dim(Other)[2])]<-c(paste(bl[j], "_mean", sep=""), paste(bl[j], "_std", sep=""))
+  }
+  
+  colnames(Other)<-gsub(" ", "_", colnames(Other))
+
+  
   
   cn<-colnames(CPUEsd)
-  for (j in 1:length(cn)){ cn[j]<-paste(cn[j], "_std", sep="")  }
+  for (j in 3:length(cn)){ cn[j]<-paste(cn[j], "_std", sep="")  }
   colnames(CPUEsd)<-cn
   
   cn<-colnames(CPUEmean)
-  for (j in 1:length(cn)){ cn[j]<-paste(cn[j], "_mean", sep="")  }
+  for (j in 3:length(cn)){ cn[j]<-paste(cn[j], "_mean", sep="")  }
   colnames(CPUEmean)<-cn
   
   #' combine all data sets into one df
-  means<-cbind(CPUEmean, Other[grep("_mean_", colnames(Other))])
-  stds<-cbind(CPUEsd, Other[grep("_std_", colnames(Other))])
+  means<-full_join(CPUEmean, cbind(Other[1], Other[grep("_mean", colnames(Other))]), by="year")
+  stds<-full_join(CPUEsd, cbind(Other[1], Other[grep("_std", colnames(Other))]), by="year")
+  #means<-cbind(CPUEmean, Other[grep("_mean", colnames(Other))])
+  #stds<-cbind(CPUEsd, Other[grep("_std", colnames(Other))])
   
   #' identify 2 varibles with highest PC1 ranks
   PCAtable<-arrange(PCAtable, pc1)
   High.rank<- PCAtable$variable[c(1,2,length(PCAtable$variable),length(PCAtable$variable) -1 ) ]
   
   #' make combined data set of High Ranked variables and StDev for plotting
-  HR1<-cbind(means$Year_mean, means[grep(High.rank[1], colnames(means))], stds[grep(High.rank[1], colnames(means))])
+  HR1<-cbind(means$year, means[grep(High.rank[1], colnames(means))], stds[grep(High.rank[1], colnames(means))])
   HR1$HR<-c(1)
   HR1$variable<-colnames(HR1[2])
   colnames(HR1)<-c("year", "data", "StDev", "HR", "variable")
   
-  HR2<-cbind(means$Year_mean, means[grep(High.rank[2], colnames(means))], stds[grep(High.rank[2], colnames(means))])
+  HR2<-cbind(means$year, means[grep(High.rank[2], colnames(means))], stds[grep(High.rank[2], colnames(means))])
   HR2$HR<-c(2)
   HR2$variable<-colnames(HR2[2])
   colnames(HR2)<-c("year", "data", "StDev", "HR", "variable")
   
-  HR3<-cbind(means$Year_mean, means[grep(High.rank[3], colnames(means))], stds[grep(High.rank[3], colnames(means))])
+  HR3<-cbind(means$year, means[grep(High.rank[3], colnames(means))], stds[grep(High.rank[3], colnames(means))])
   HR3$HR<-c(3)
   HR3$variable<-colnames(HR3[2])
   colnames(HR3)<-c("year", "data", "StDev", "HR", "variable")
   
-  HR4<-cbind(means$Year_mean, means[grep(High.rank[4], colnames(means))], stds[grep(High.rank[4], colnames(means))])
+  HR4<-cbind(means$year, means[grep(High.rank[4], colnames(means))], stds[grep(High.rank[4], colnames(means))])
   HR4$HR<-c(4)
   HR4$variable<-colnames(HR4[2])
   colnames(HR4)<-c("year", "data", "StDev", "HR", "variable")
@@ -84,6 +119,8 @@ for (i in 1:length(area)){  # not the same number of StDev and mean columns
   
 }
   
+
+
 
 #' Extract RANK from PCA table and add it to a common PCA table
 for (i in 1:length(area)){  # not the same number of StDev and mean columns
@@ -120,24 +157,24 @@ class(TRank)<-"numeric"
 TRank1<-TRank[grep(1, rownames(TRank)),]
 TRank2<-TRank[grep(2, rownames(TRank)),]
 
-calculate average rank, median rank etc
+#calculate average rank, median rank etc
 #BoxPlot Rank
 
-png("Rank boxplot PC1.png")
-par(mai=c(2.8,1,1,1), cex=0.8)
+png("Rank boxplot PC1.png", width=1000, height=1000)
+par(mai=c(4,1,1,1), cex=1.4)
 boxplot(TRank1, las=2)
 title("Absolute rank PC1")
 dev.off()
 
-png("Rank boxplot PC2.png")
-par(mai=c(2.8,1,1,1), cex=0.8)
+png("Rank boxplot PC2.png", width=1000, height=1000)
+par(mai=c(4,1,1,1), cex=1.4)
 boxplot(TRank2, las=2)
 title("Absolute rank PC2")
 dev.off()
 
 write.csv(TRank, "Rank tables PCA loadings.csv")
 
-
+boxplot.stats(TRank2)
 
 
 #'------------------ REST NOT USED --------
